dfcb1deb6797924fb5647c907b66f31956a0768a
konstantikkov <60280866+konstantikkov@users.noreply.github.com>
2021-09-19 16:48:00 +0300

    express refactoring

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ const config = require('config')
 
 const app = express()
 
+app.use(express.json({ extended: true }))
+
 if(process.env.NODE_ENV==='production'){
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
